edd1e1ffe5a57886939fc9b4ab6d85761cfdfd67
Lazizjon-cloud <l9561997@gmail.com>
2026-01-10 02:40:54 +0300

    Update base path in vite.config.ts
    Set base path to match repository name.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Car-Model/",  // <--- THIS IS THE FIX. It matches your repository name.
   define: {
     'process.env': process.env
   }
